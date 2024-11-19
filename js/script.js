3f958f915b2c8bc820589d0124d9d7c42d3aa7e0
Hemanth Reddy Allugunti <93366548+alluguntihemanth@users.noreply.github.com>
2024-11-19 22:24:07 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,9 @@
 // script.js
-
 function updateTime() {
     const timeElement = document.getElementById('time');
-    const now = new Date();
-    const options = { 
-        hour: '2-digit', 
-        minute: '2-digit', 
-        second: '2-digit', 
-        hour12: true, 
-        timeZone: 'America/Denver' // Salt Lake City timezone
-    };
-    
-    const timeString = now.toLocaleTimeString('en-US', options) + ' MDT';
+    const currentDate = new Date();
+    const timeString = currentDate.toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' });
     timeElement.textContent = timeString;
 }
 
-// Update time every second
 setInterval(updateTime, 1000);
-updateTime();
